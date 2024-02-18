a13950dd290633b5744422f9c7d22f172a90d94d
Nandan_9 <a.d.devnandan@gmail.com>
2024-02-18 15:53:17 +0530

    db setup

--- AS2/settings.py
+++ AS2/settings.py
@@ -79,7 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'userlogindb',
-        'PASSWORD': 'admin',
+        'user': 'devan',
+        'PASSWORD': 'Devnandan@2003',
         'HOST':'localhost',
         'PORT':'3306',
     }
